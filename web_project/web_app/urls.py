5c460f9f4fdeead51249126333fd5b5b3971d41f
Студент группы IS <is@sielom.local>
2025-12-05 09:44:59 +0500

    modified

--- web_project/web_app/urls.py
+++ web_project/web_app/urls.py
@@ -4,6 +4,7 @@ from web_app import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
+    path('dish/<int:dish_id>/', views.dish_details, name='dish_details'),
     path('reservation/', views.reservation, name='reservation'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
--- web_project/web_app/views.py
+++ web_project/web_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -9,15 +9,24 @@ def home(request):
 
 def menu(request):
     search = request.GET.get('q', '') 
+    category = request.GET.get('category', '')
     dishes = Menu.objects.filter(is_available=True)
     
     if search:
         dishes = dishes.filter(dish_name__icontains=search)
+
+    if category:
+        dishes = dishes.filter(dish_name__icontains=search)
     
     return render(request, 'menu.html', {
         'dishes': dishes,
+        'category': category,
         'search': search 
     })
+
+def dish_details(request, dish_id):
+    dish = get_object_or_404(Menu, id=dish_id, is_available=True)
+    return render(request, 'dish_details.html', {'dish': dish})
 
 def reservation(request):
     available_tables = Tables.objects.filter(status='free')
